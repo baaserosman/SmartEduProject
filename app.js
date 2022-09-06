c9c8645f05bb0241e39a15fa91bb41ea28d5e5d3
baaserosman <baaser.osman@gmail.com>
2022-09-06 17:28:08 +0200

    3rd commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pageRoute = require('./routes/pageRoute');
 
 const app = express();
 
@@ -9,16 +10,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //* ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: 'index'
-  });
-});
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: 'about'
-  });;
-});
+app.use('/', pageRoute);
+
+
 
 
 
