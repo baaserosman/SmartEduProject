1acdc6088afd07b892ede6a6bac5c57b65ecf664
baaserosman <baaser.osman@gmail.com>
2022-09-09 07:23:18 +0200

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
   try {
@@ -8,6 +9,27 @@ exports.createUser = async (req, res) => {
       status: 'success',
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+      if (same) {
+        // USER SESSION
+          res.status(200).send('YOU ARE LOGGED IN');
+        }
+      });
+    };
   } catch (error) {
     res.status(400).json({
       status: 'fail',
